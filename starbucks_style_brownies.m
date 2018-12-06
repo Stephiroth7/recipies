a3fbb01a09e5146ead04a3537fcd5ab3dfd5bb3e
Stephany Floyd <stephany.floyd@hotmail.com>
2018-12-05 22:26:11 -0600

    create sbux_brownies

---
+++ starbucks_style_brownies.m
@@ -0,0 +1,31 @@
+# Brownies
+
+**INGREDIENTS**
+
+* 1 1/2 cup unsalted butter, melted
+* 3 cup granulated sugar
+* 6 eggs
+* 4 teaspoons vanilla extract
+* 1 1/2 cup all-purpose flour
+* 1 1/2 cup unsweetened natural cocoa powder
+* 3/4 teaspoon salt, or to taste
+* 1 1/2 cups semi-sweet chocolate chunks, divided equally
+
+**PREPARATION:**
+
+Preheat oven to 350F and spray a half sheet (18" x 13") baking pan with cooking spray.
+
+1. Add the butter to a large, microwave safe bowl and heat on high power to melt, about 1 minute.
+2. Add the sugar, eggs, vanilla, and whisk to combine.
+3. Add the cocoa powder and stir to combine 
+4. Add the flour, salt, and stir to combine.
+5. Add half chocolate chunks and stir to combine.
+6. Turn batter out into prepared pan (it will be thick) and smooth the top lightly with a spatula.
+7. Evenly sprinkle remaining chocolate chunks on top, lightly pushing them down so they adhere.
+8. Bake for about 30 - 35 minutes or until the edges are set and a toothpick inserted in the center comes out clean or with a few moist crumbs, but no batter. 
+
+
+NOTES:
+* use the baking spray with flour added, or add flour after spraying to help them release.
+* If th cocoa powder is particularly lumpy, sift it to get the lumps out.  The cocoa powder may remain as dry lumps otherwise.
+* Allow brownies to cool in pan on a wire rack for about 2 hours before slicing and serving.
